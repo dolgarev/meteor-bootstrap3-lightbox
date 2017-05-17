4eec4dc774172a1328fdd8ceb07f1eaf791dce73
dolgarev <o.dolgarev@gmail.com>
2017-05-17 13:00:37 +0300

    added some fixes

--- package.js
+++ package.js
@@ -1,16 +1,14 @@
 Package.describe({
-  summary: "Bootstrap 3 Lightbox from @ashleydw, packaged for Meteor.js",
-  version: "0.2.0",
-  git: "https://github.com/tsega/meteor-bootstrap3-lightbox.git"
+  name: 'liberation:bootstrap3-lightbox',
+  version: '0.0.1',
+  summary: 'Bootstrap 3 Lightbox from @ashleydw, packaged for Meteor.js',
+  git: 'https://github.com/dolgarev/meteor-bootstrap3-lightbox.git'
 });
 
 Package.on_use(function (api, where) {
-  api.versionsFrom('METEOR@0.9.0');
-
+  api.versionsFrom('1.4.2.7');
+  api.use(['ecmascript', 'less']);
   api.use('jquery', 'client');
-
-  api.add_files([
-    'lib/css/ekko-lightbox.min.css',
-    'lib/js/ekko-lightbox.min.js'
-    ], 'client');
+  api.addFiles('lib/css/ekko-lightbox.less', 'client');
+  api.mainModule('lib/js/ekko-lightbox.js', 'client');
 });
