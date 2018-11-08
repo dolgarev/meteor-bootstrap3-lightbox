b317635cb2aa3bed8ca318ee8be9976ecf302e73
dolgarev <o.dolgarev@gmail.com>
2018-11-08 12:23:37 +0200

    fixed width for bootstrap4-alpha

--- package.js
+++ package.js
@@ -1,14 +1,13 @@
 Package.describe({
   name: 'liberation:bootstrap3-lightbox',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Bootstrap 3 Lightbox from @ashleydw, packaged for Meteor.js',
   git: 'https://github.com/dolgarev/meteor-bootstrap3-lightbox.git'
 });
 
 Package.on_use(function (api, where) {
   api.versionsFrom('1.4.2.7');
-  api.use(['ecmascript', 'less']);
-  api.use('jquery', 'client');
+  api.use(['ecmascript', 'jquery', 'less'], 'client');
   api.addFiles('lib/css/ekko-lightbox.less', 'client');
   api.mainModule('lib/js/ekko-lightbox.js', 'client');
 });
